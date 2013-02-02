328eb7592d301d13991ccf4fbb97b4bf81f091cc
maxLoveCode <maxkidy@gmail.com>
2013-02-02 13:15:45 +0000

    added whatsnew class
    whatsnew page uses tableview, its uncompleted yet

---
+++ SSPI/SSPI/Whatsnew.h
@@ -0,0 +1,13 @@
+//
+//  Whatsnew.h
+//  SSPI
+//
+//  Created by Cheng Ma on 02/02/2013.
+//  Copyright (c) 2013 COOMKO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Whatsnew : UITableViewController
+
+@end
