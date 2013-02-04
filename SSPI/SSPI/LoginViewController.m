ba3002408637919105af64a0a8acd1d1f2564b1e
maxLoveCode <maxkidy@gmail.com>
2013-02-04 18:57:43 +0000

    Revert "Add LoginViewPage, the login button action haven't been finished yet"
    This reverts commit b18a9246e64a596a085be3513a2a884fa1d5fd38.

--- SSPI/SSPI/LoginViewController.m
+++ SSPI/SSPI/LoginViewController.m
@@ -2,11 +2,7 @@
 //  LoginViewController.m
 //  SSPI
 //
-<<<<<<< HEAD
 //  Created by Hannah Oliver on 04/02/2013.
-=======
-//  Created by Cheng Ma on 04/02/2013.
->>>>>>> Add LoginViewPage, the login button action haven't been finished yet
 //  Copyright (c) 2013 COOMKO. All rights reserved.
 //
 
@@ -18,12 +14,6 @@
 
 @implementation LoginViewController
 
-<<<<<<< HEAD
-=======
-@synthesize txtPassword,txtUsername,loginClicker,signUpClicker;
-
-
->>>>>>> Add LoginViewPage, the login button action haven't been finished yet
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -36,11 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-<<<<<<< HEAD
 	// Do any additional setup after loading the view.
-=======
-    // Do any additional setup after loading the view from its nib.
->>>>>>> Add LoginViewPage, the login button action haven't been finished yet
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,17 +35,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-<<<<<<< HEAD
-=======
-- (IBAction)loginClicker:(id)sender
-{
-    //incompleted for the login button
-}
-
-- (IBAction)signUpClicker:(id)sender
-{
-    //incompleted for the signup button
-}
-
->>>>>>> Add LoginViewPage, the login button action haven't been finished yet
 @end
